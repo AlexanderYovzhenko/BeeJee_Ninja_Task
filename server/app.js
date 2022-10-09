ac869a7aef5aa34639763c219c30685d1f981c5e
Alexander <renyreny125tt@gmail.com>
2022-10-09 22:25:32 +0300

    feat: add routes

--- server/app.js
+++ server/app.js
@@ -3,13 +3,13 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const routesLogin = require('./src/login/login')
-const routes = require('./src/routes')
+const routesTasks = require('./src/routes')
 
 app.use(cors())
 
 app.use(bodyParser.json())
 
 app.use('', routesLogin)
-app.use('', routes)
+app.use('', routesTasks)
 
 module.exports = app
