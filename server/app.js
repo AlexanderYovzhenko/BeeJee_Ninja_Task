76dc47e2e24a531eb20e299e1b0a7aa6ec0d2d44
Alexander <renyreny125tt@gmail.com>
2022-10-09 21:01:56 +0300

    feat: add route login get jwt token

--- server/app.js
+++ server/app.js
@@ -2,9 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const routesLogin = require('./src/login/login')
+const routes = require('./src/routes')
 
 app.use(cors())
 
 app.use(bodyParser.json())
+
+app.use('', routesLogin)
+app.use('', routes)
 
 module.exports = app
