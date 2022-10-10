54dfec75dca9555b56c7878765d9d874b94257f1
Alexander <renyreny125tt@gmail.com>
2022-10-10 23:24:22 +0300

    feat: add constants

---
+++ client/src/constants/queryVariables.js
@@ -0,0 +1,9 @@
+const URL_SERVER = 'https://server-beejee.herokuapp.com';
+const OK = 200;
+const CREATED = 201;
+const NoContent = 204;
+const UNAUTHORIZED = 401;
+const FORBIDDEN = 403;
+const CONFLICT = 409;
+
+export { URL_SERVER, OK, CREATED, FORBIDDEN, CONFLICT, UNAUTHORIZED, NoContent };
