7d890782afac0cfad19db7cd9db26b5b57e4ce9d
Alexander <renyreny125tt@gmail.com>
2022-10-10 20:22:00 +0300

    feat: add router

---
+++ client/src/router/AppRouter.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from '../pages/login-page/LoginPage';
+import Page404 from '../pages/page-404/Page404';
+
+const AppRouter = () => {
+
+  return (
+    <Routes>
+      {/* <Route path="/" element={<ToDoList />} /> */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
+  );
+};
+
+export default AppRouter;
