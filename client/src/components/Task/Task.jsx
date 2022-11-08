ccfffdd4771e5b5cf09bc5e76de1da7ea63587c4
Alexander <renyreny125tt@gmail.com>
2022-11-08 10:10:34 +0300

    feat: add example view task

--- client/src/components/Task/Task.jsx
+++ client/src/components/Task/Task.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addTask, updateTask } from '../../api/tasks';
 
-const Task = () => {
+const Task = (props) => {
+  const dispatch = useDispatch();
+  const { task } = props;
+
   return (
     <div className='Task'>
-      <h1>title</h1>
+      {task.id}
+      <h1>{task.name}</h1>
+      <h2>{task.email}</h2>
+      <p>{task.description}</p>
+      <p>{task.executed ? "true" : "false"}</p>
+      <button className="button-update-task" onClick={()=> dispatch(updateTask(task.id))}>Редактировать задачу</button>
     </div>
   );
 };
